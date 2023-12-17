c820fd980b10519c737a2054d7cd990721e15cba
Gregalba <152832683+Gregalba@users.noreply.github.com>
2023-12-17 14:38:21 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Login Functionality
+    const loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
     // Product Add to Cart functionality
     const addToCartButtons = document.querySelectorAll('.product button');
     addToCartButtons.forEach(btn => {
